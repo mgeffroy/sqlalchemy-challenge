06d6a69bc721dbcef92bd32e7e96480de4853642
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-06-02 19:45:13 -0500

    Added resources file

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 #Import dependencies including  flask
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-import datetime as dt
 from flask import Flask , jsonify
 
 
 # Database Setup
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect =True)
 
 # Save reference to the table
-Station = Base.classes.Station
-Measurement = Base.classes.Measurement
+
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 
 # Flask setup 
@@ -38,10 +37,7 @@ def home():
       f"/api/v1.0/precipitation<br/>"
       f"/api/v1.0/tobs<br/>"
        
-       
-    )
+       )
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
